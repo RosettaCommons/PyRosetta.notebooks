4d2c74b05374a383b86cbdecfe31efb92b45b22d
kathyl <kle16@jhu.edu>
2020-09-04 12:53:20 -0700

    removed test.bash

--- test.bash
+++
@@ -1,8 +0,0 @@
-# Edit TOC and index so that links are to the student versions
-sed 's/\/notebooks\//\/student-notebooks\//g' toc.ipynb > _toc.ipynb
-sed 's/\/notebooks\//\/student-notebooks\//g' index.ipynb > _index.ipynb
-
-# Remove old toc file and index file
-rm toc.ipynb
-rm index.ipynb
-
